3bd9583ed84fce6422add5757917f76c7bfe7a13
Georgios Politis <gp@superpointer.com>
2023-06-25 11:26:20 +0200

    Initial commit.

---
+++ txt2ics.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+from icalendar import Calendar, Todo
+import dateutil.parser 
+import sys
+import argparse
+import re
+
+#import logging
+#logging.basicConfig()
+#logging.getLogger().setLevel(logging.DEBUG)
+
+STATUS_REGEX = r"^- \[( |x)\]"
+# https://www.rfc-editor.org/rfc/rfc5545#section-3.8.1.11
+KEYWORD_REGEX = r"^- (TODO|DONE|CANCELLED|NEEDS-ACTION|COMPLETED|IN-PROCESS)"
+KEYWORD_MAP = {'TODO': 'NEEDS-ACTION', 'DONE': 'COMPLETED'}
+
+def make_todo(line):
+    rematch = re.match(STATUS_REGEX, line)
+    if not rematch:
+        rematch = re.match(KEYWORD_REGEX, line)
+        if not rematch:
+            return
+        else:
+            status = rematch.group(1)
+            if status in KEYWORD_MAP:
+                status = KEYWORD_MAP[status]
+    else:
+        status = "COMPLETED" if rematch.group(1) == "x" else "NEEDS-ACTION"
+
+
+    summary = line[len(rematch.group(0)):].strip()
+    if len(summary) == 0:
+        return
+    
+    todo = Todo()
+    todo["status"] = status
+    todo["summary"] = summary
+    return todo
+
+def make_calendar(infile):
+    cal = Calendar()
+    for line in infile:
+        todo = make_todo(line)
+        if todo:
+            cal.add_component(todo)
+    return cal
+
+def txt2ics(args):
+    cal = make_calendar(args.infile)
+    args.outfile.write(cal.to_ical().decode("utf-8"))
+
+parser = argparse.ArgumentParser(description='Converts TODOs in a text file into an iCal file.')
+parser.add_argument('--infile', type=argparse.FileType('r'))
+parser.add_argument('--outfile', nargs='?', type=argparse.FileType('w'), default=sys.stdout)
+
+txt2ics(parser.parse_args())
