c4fd4e51973f88c3265a0c944d17641ecc3b423a
George Politis <gp@jitsi.org>
2023-08-10 22:47:20 -0400

    Fixes ModuleNotFoundError when running the tests.

--- tests/test_lib.py
+++ tests/test_lib.py
@@ -4,9 +4,9 @@ from txt2ics import make_calendar
 class BasicTestCase(unittest.TestCase):
 
     def test_make_calendar(self):
-        with open(file="tests/todo.md", mode="r", encoding="utf-8") as todofile:
+        with open(file="../tests/todo.md", mode="r", encoding="utf-8") as todofile:
             cal = make_calendar(todofile)
-            with open("tests/todo.ics", 'rb') as icsfile:
+            with open("../tests/todo.ics", 'rb') as icsfile:
                 assert cal.to_ical() == icsfile.read()
 
 if __name__ == '__main__':
